f888dd7d5c09818c0cd6e64995fdbd583f87ea8f
Prashantkr81 <Prashantkr81@outlook.com>
2026-08-17 20:30:51 +0530

    fixes

--- src/services/firebase/config.ts
+++ src/services/firebase/config.ts
@@ -1,43 +1,49 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
+
 import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
+
+import {
+  initializeAuth,
+  getReactNativePersistence,
+} from 'firebase/auth';
+
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+  apiKey:
+    process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+
+  authDomain:
+    process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+
+  projectId:
+    process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+
+  storageBucket:
+    process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+
+  messagingSenderId:
+    process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+
+  appId:
+    process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
 };
 
-const app = initializeApp(firebaseConfig);
+const app = initializeApp(
+  firebaseConfig,
+);
 
-const createAuth = () => {
-  try {
-    // require at runtime to avoid bundler/type resolution issues
-    // eslint-disable-next-line @typescript-eslint/no-var-requires
-    const authNative = require('firebase/auth/react-native');
+export const auth = initializeAuth(
+  app,
+  {
+    persistence:
+      getReactNativePersistence(
+        AsyncStorage,
+      ),
+  },
+);
 
-    if (
-      authNative &&
-      typeof authNative.initializeAuth === 'function' &&
-      typeof authNative.getReactNativePersistence === 'function'
-    ) {
-      return authNative.initializeAuth(app, {
-        persistence: authNative.getReactNativePersistence(AsyncStorage),
-      });
-    }
-  } catch (e) {
-    // fall back to web auth
-  }
-
-  return getAuth(app);
-};
-
-export const auth = createAuth();
-
-export const db = getFirestore(app);
+export const db =
+  getFirestore(app);
 
 export default app;
